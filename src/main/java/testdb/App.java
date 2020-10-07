0fe8e638f50d7e0bf91886db98956dc1ca8a59a8
Marco Hassler <marhassler@tsn.at>
2020-10-07 16:46:47 +0200

    unfinished exercise 2

--- src/main/java/testdb/App.java
+++ src/main/java/testdb/App.java
@@ -1,9 +1,7 @@
 package testdb;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Calendar;
 
 /**
  * Hello world!
@@ -11,16 +9,39 @@ import java.sql.Statement;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws SQLException, ClassNotFoundException {
+        java.sql.Date date = new java.sql.Date(Calendar.getInstance().getTime().getTime());
+        insertInvoice(date,"bestellung 4",12, (byte) 1,connectDatabase());
+    showInvoices(connectDatabase());
+    }
+    public static Connection connectDatabase() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/jdbctest","root","");
+        return con;
+    }
+    public static void showInvoices(Connection con)
     {
         try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test","root","1234");
             Statement stmt=con.createStatement();
-            ResultSet rs=stmt.executeQuery("select * from Produkte");
+            ResultSet rs=stmt.executeQuery("select * from Invoices");
             while(rs.next())
-                System.out.println(rs.getInt(1)+"  "+rs.getString(2));
+                System.out.println(rs.getInt(1)+"  "+rs.getString(2) + " "+rs.getString(3)+ " "+rs.getInt(4));
             con.close();
         }catch(Exception e){ System.out.println(e);}
+    }
+    public static void insertInvoice(Date date, String description, double value, Byte paid, Connection con) throws SQLException {
+        Statement stmt=con.createStatement();
+        String sql = "insert into invoices"
+                   + "(date,description,value,paid)"
+                    + "values (date,description,value,paid)";
+        stmt.executeQuery(sql);
+    }
+    public static void updateInvoice(int id, Date date, String description, double value, int paid)
+    {
+
+    }
+    public static void deleteInvoice(int id)
+    {
+
     }
 }
