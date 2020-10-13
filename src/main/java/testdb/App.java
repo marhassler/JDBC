bde867ffff098dff192da1313849e631cfeb5095
Marco Hassler <marhassler@tsn.at>
2020-10-13 16:08:09 +0200

    finished exercise

--- src/main/java/testdb/App.java
+++ src/main/java/testdb/App.java
@@ -7,7 +7,9 @@ public class App
 {
     public static void main( String[] args ) throws SQLException, ClassNotFoundException {
         java.sql.Date date = new java.sql.Date(Calendar.getInstance().getTime().getTime());
-        insertInvoice(date,"bestellung 4",12, (byte) 1,connectDatabase());
+        insertInvoice(date,"bestellung 231",324, (byte) 1,connectDatabase());
+        updateInvoice(1,date,"bestellung 1 neu",192, (byte) 1,connectDatabase());
+        deleteInvoice(9,connectDatabase());
     showInvoices(connectDatabase());
     }
     public static Connection connectDatabase() throws ClassNotFoundException, SQLException {
@@ -25,19 +27,32 @@ public class App
             con.close();
         }catch(Exception e){ System.out.println(e);}
     }
-    public static void insertInvoice(Date date, String description, double value, Byte paid, Connection con) throws SQLException {
-        Statement stmt=con.createStatement();
-        String sql = "insert into invoices"
-                   + "(date,description,value,paid)"
-                    + "values ('2020-10-12','bestellung 6','42','0')";
-        stmt.executeUpdate(sql);
-    }
-    public static void updateInvoice(int id, Date date, String description, double value, int paid)
-    {
+    public static void insertInvoice(Date date, String description, int value, Byte paid, Connection con) throws SQLException {
 
-    }
-    public static void deleteInvoice(int id)
-    {
+        PreparedStatement insert = con.prepareStatement
+                ("insert into invoices  (date, description, value, paid) values(?,?,?,?)");
+        insert.setDate(1, date);
+        insert.setString(2, description);
+        insert.setInt(3, value);
+        insert.setByte(4, paid);
 
+        insert.executeUpdate();
+    }
+    public static void updateInvoice(int id, Date date, String description, int value, Byte paid, Connection con) throws SQLException {
+        PreparedStatement update = con.prepareStatement
+                ("UPDATE invoices SET date = ?, description = ?, value = ?, paid = ? WHERE id = ?");
+        update.setDate(1, date);
+        update.setString(2, description);
+        update.setInt(3, value);
+        update.setByte(4, paid);
+        update.setInt(5, id);
+
+        update.executeUpdate();
+    }
+    public static void deleteInvoice(int id,Connection con) throws SQLException {
+        PreparedStatement update = con.prepareStatement
+                ("DELETE FROM invoices WHERE ID = ? ");
+        update.setInt(1, id);
+        update.executeUpdate();
     }
 }
